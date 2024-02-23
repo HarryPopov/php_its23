d6369a92679c645628c7c6ed349db97560439a15
HarryPopov <harry.popov2@gmail.com>
2024-02-23 11:32:29 +0200

    asd

--- 01.php
+++ 01.php
@@ -1,172 +1,39 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="et">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DoPHP harjutused</title>
-</head>
-<body>
-    <h1>Harjutus 04</h1>
-    <?php
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
 
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
 
-    $p = 10;
-    switch ($p) {
-        case ($p > 9):
-            echo "SUPER!";
-            break;
+    <body>
+        <div class="container">
+           <?php
+           echo "tere maailm";
+           ?>
+        </div>
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
 
-            case($p>=5 && $p<=9):
-            echo "TEHTUD!";
-            break;
-            
-            case ($p<5):
-                echo "KASIN!";
-                break;
-        
-        default:
-            echo "Punkte pole lisatud";
-            break;
-    }
-
-
-
-    ?>
-
-
-    <form action="" method="GET">
-
-    Lisa oma sünniaasta: <input type="number" name="synd"
-    min="1900" max="2100">
-    <input type="submit" value="Leia juubel"><br>
-    </form>
-    <?php
-    if (!empty($_GET['synd'])) {
-        $aasta = $_GET['synd'];
-        $seeaasta = date('Y');
-        $vanus = $seeaasta - $aasta;
-        if ($vanus % 5 == 0) {
-            echo "Sul on juubel";
-        }
-        else {
-            echo "Sul ei ole juubel";
-        }
-    }
-
-    // var_dump on testimiseks näitab väärtusi mille ta saab
-
-    ?>
-
-
-
-
-
-
-
-    <?php
-    $nr1 = 2;
-    $nr2 = 1;
-    //  (ruuduga if) võimalused < > >= <= == != True/False
-    // OR (||) -> Peab olema üks või teine õige
-    // AND (&&) -> Peavad olema mõlemad õiged
-    if ($nr2 > 0 && $nr1 != 0) {
-        $tehe = $nr1 / $nr2;
-        echo $tehe;
-    } 
-    else {
-        echo "Nulliga ei saa jagada";
-    }
-
-
-    ?>
-
-
-<h1>Harjutus 03</h1>
-<!-- action > fail kuhu saadetakse
-     method > Kuidas saadetakse, GET on avalik, POST on peidetud
--->
-<form action="" method="GET">
-  Toode 1: <input type="number" name="toode1"><br>
-  Toode 2: <input type="number" name="toode2"><br>
-  Toode 3: <input type="number" name="toode3"><br>
-
-  <input type="submit" value="Arvuta">
-
-</form>
-<?php
-if (!empty($_GET['toode1']) &&
-!empty($_GET['toode2']) &&
-!empty($_GET['toode3'])) {
-    # code...
-
-//  GETiga saatsin, GETiga võtan
-$toode1 = $_GET['toode1'];
-$toode2 = $_GET['toode2'];
-$toode3 = $_GET['toode3'];
-$kokku = $toode1 + $toode2 + $toode3;
-
-echo $toode1. "<br>";
-echo $toode2. "<br>";
-echo $toode3. "<br>";
-
-echo "Toode1: $toode1 tk<br>";
-echo "Toode1: $toode2 tk<br>";
-echo "Toode1: $toode3 tk<br>";
-echo "Kõik kokku: $kokku tk";
-}
-
-?>
-
-
-<?php
-
-
-
-?>
-
-
-    <h1>Harjutused 02</h1>
-    <?php
-    // %0.2 (võtab 2 komakohta)
-
-    $arv1 = 5;       // int
-    $arv2 = 3.14;    // float
-    $tehe = $arv1 / $arv2;    // / jagamine
-    $jaak = $arv1 % $arv2;    // % leiab jäägi
-    echo "Vastus: $tehe<br>";
-    printf("vastus: %d / %0.2f = %0.2f <br>", $arv1, $arv2, $tehe);
-    echo "Jääk:" .$jaak;
-    ?>
-
-  <?php
-  /*
-  (see on mitme realine kommentaar)
-Harjutus 01
-8.2.24
-Lihtsalt harjutan
-
-  */
-//   (see oleks üherealine kommentaar ctrl+ä)
-
-      $enimi = "Harry";
-      $pnimi = "Popov";
-      $vanus = 34;
-      $pikk = 172; 
-      $kaal = "86";
-      $hyydnimi = "ENERGIA";
-      $lugu = 'Hunt - "s\'hooters"';
-    
-    //  see tingmärk  \ - ignoreerib
-    //  <p></p> määrab lõigud vmsellsit
-
-    //   $enimi = "Horry"; see ei ole konstantne ja seda väärtust saab muuta (ta võtab alati viimase)
-
-     echo "<p> Tere $enimi, hüüdnimega \"$hyydnimi\" $pnimi <br>";
-     echo "Oled $vanus aastat vana <br>";
-     echo "Kuradi jumbu oled vaid $pikk cm pikkune!<br>";
-     echo "Kaaluga peaksid ka midagi ette võtma $kaal kg vaalaskala<br>";
-     echo 'Lemmik lugu on: '.$lugu.'</p>'; // (.) punkt on kokkuliitmiseks
-
-   ?>
-</body>
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
